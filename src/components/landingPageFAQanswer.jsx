54f53ab5b52be33bea9be62c85f4306ccf2ee855
Bratamalya Das Gupta <bratamalya2@gmail.com>
2024-08-09 12:03:29 +0530

    fix(): fixed null case for landingpage FAQ

--- src/components/landingPageFAQanswer.jsx
+++ src/components/landingPageFAQanswer.jsx
@@ -1,5 +1,5 @@
 export default function LandingPageFAQAnswer({ answer, currentQuestion }) {
-    if (!currentQuestion)
+    if (currentQuestion === null)
         return null;
     return <ul className="bg-[#FEEECB] rounded-[36px] py-[21px] px-[44px] flex flex-col gap-[8px] mb-[24px] list-disc">
         {
